89f310eebcc6b80f65a1e0c8ee76d7320f87c060
yeqichao230516 <15294744454@163.com>
2026-01-27 08:53:33 +0800

    feat:address_book

--- pkg/feishu/bitable/record.go
+++ pkg/feishu/bitable/record.go
@@ -1,1 +1,25 @@
 package feishu_bitable
+
+import (
+	"context"
+	"fmt"
+
+	lark "github.com/larksuite/oapi-sdk-go/v3"
+	larkcontact "github.com/larksuite/oapi-sdk-go/v3/service/contact/v3"
+)
+
+func GetUserNameFromUnionID(unionID string, client *lark.Client) (string, error) {
+	req := larkcontact.NewGetUserReqBuilder().
+		UserId(unionID).
+		UserIdType(`union_id`).
+		Build()
+
+	resp, err := client.Contact.V3.User.Get(context.Background(), req)
+	if err != nil {
+		return "", fmt.Errorf("请求失败: %w", err)
+	}
+	if !resp.Success() {
+		return "", fmt.Errorf("请求失败: %s", resp.CodeError.Msg)
+	}
+	return *resp.Data.User.Name, nil
+}
