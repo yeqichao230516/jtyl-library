48a42984b70ff2c7cff5cb5ae7f7a37995684b67
yeqichao230516 <15294744454@163.com>
2026-01-30 13:52:10 +0800

    feat:webhook

---
+++ pkg/feishu/webhook/decrypt.go
@@ -0,0 +1,42 @@
+package feishu_webhook
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/base64"
+	"strings"
+)
+
+// AES-CBC 解密
+func Decrypt(encrypt, key string) string {
+	buf, err := base64.StdEncoding.DecodeString(encrypt)
+	if err != nil {
+		return ""
+	}
+	if len(buf) < aes.BlockSize {
+		return ""
+	}
+	keyBs := sha256.Sum256([]byte(key))
+	block, err := aes.NewCipher(keyBs[:sha256.Size])
+	if err != nil {
+		return ""
+	}
+	iv := buf[:aes.BlockSize]
+	buf = buf[aes.BlockSize:]
+
+	if len(buf)%aes.BlockSize != 0 {
+		return ""
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(buf, buf)
+	n := strings.Index(string(buf), "{")
+	if n == -1 {
+		n = 0
+	}
+	m := strings.LastIndex(string(buf), "}")
+	if m == -1 {
+		m = len(buf) - 1
+	}
+	return string(buf[n : m+1])
+}
